32221d23a2c9e0ffeb9c27dda481a0817e13fb7d
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-10-06 18:49:36 +0800

    completed edit and delete in /travelers

--- src/routes/api/register/traveler/+server.ts
+++ src/routes/api/register/traveler/+server.ts
@@ -1,7 +1,6 @@
 import { json } from "@sveltejs/kit";
 import db from "$db/mongo";
-import { gen_primary_key } from "../../../../utils/IDGenerator.js";
-import { travelers } from "../../../../stores/Travelers.js";
+import { gen_primary_key } from "../../../../utils/IDGenerator";
 export async function POST({ request, cookies }) {
     const _db = db.collection("Traveler");
     const _travelers = await _db.find({}).toArray();
@@ -12,14 +11,12 @@ export async function POST({ request, cookies }) {
             { traveler_id: data.traveler_id },
             { $set: { ...data } }
         );
-        travelers.set(await _db.find({}).toArray());
         console.log("db says:", result);
         return json(result, { status: 201 });
     }
     const traveler_id =
         _travelers.length > 0 ? gen_primary_key(_travelers) : "T1001";
     const result = await _db.insertOne({ traveler_id, ...data });
-    travelers.set(await _db.find({}).toArray());
     return json(result, { status: 201 });
 }
 
@@ -28,7 +25,6 @@ export async function DELETE({ request, cookies }) {
     const data = await request.json();
     const { traveler_id } = data;
     const result = await _db.deleteOne({ traveler_id: traveler_id });
-    travelers.set(await _db.find({}).toArray());
     console.log("db says:", result);
     return json(result, { status: 201 });
 }
